395651a90ab06417475b894448d61d6b724f9140
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-01-04 04:50:06 +0000

    fix: use user icons

--- components/links.tsx
+++ components/links.tsx
@@ -1,7 +1,7 @@
 export function Github({ username }: { username: string }) {
   return (
-    <a href={`https://github.com/${username}`} target="_blank">
-      @{username}
+    <a href={`https://github.com/${username}`} target="_blank" className="flex gap-2 items-center">
+      <img src={`https://github.com/${username}.png?size=40`} alt={`avatar of ${username}`} className="size-4 rounded-full !m-0" /> @{username}
     </a>
   );
 }
